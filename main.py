62f265abdac07456c0da78699d03fdba169cb33a
Besto9945 <84808974+Besto9945@users.noreply.github.com>
2022-02-12 15:04:20 +0700

    add @app.push

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from typing import Optional
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
@@ -12,8 +12,34 @@ client = MongoClient('mongodb://localhost', 27017)
 
 db = client['miniproject']
 
+class Room(BaseModel):
+    Room: int
+    Status: int
+    Time: Optional[int] = None
 
-@app.get('/')
-def get_time(time: datetime):
-    print(time)
-    return {"time": time}
+col1 = db['Room']
+col2 = db['estimate_1']
+col3 = db['estimate_2']
+col4 = db['estimate_3']
+
+
+@app.post("/add/")
+def add_time(r: Room):
+    if r.Status == 0:
+        r.Time = datetime.now().timestamp()
+        query = {"Room": r.Room}
+        new_value = {"$set": {"Time": r.Time}}
+        col1.update_one(query, new_value)
+
+    elif r.Status == 1:
+        query = {"Room": r.Room}
+        time = col1.find_one(query, {"_id": 0,"Time": 1})
+        res = datetime.now().timestamp() - time["Time"]
+        if r.Room == 1:
+            col2.insert_one(res)
+        elif r.Room == 2:
+            col3.insert_one(res)
+        else:
+            col4.insert_one(res)
+
+
