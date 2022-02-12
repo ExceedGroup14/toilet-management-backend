ee54768aea07ff3b8968d83c86a12a2cb71a34f9
Besto9945 <84808974+Besto9945@users.noreply.github.com>
2022-02-12 16:11:54 +0700

    get time_used

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ def add_time(r: Room):
         query = {"Room": r.Room}
         new_value = {"$set": {"Time": r.Time}}
         col1.update_one(query, new_value)
+        return {
+            "result": "enter room {}".format(r.Room) 
+        }
 
     elif r.Status == 1:
         query = {"Room": r.Room}
@@ -41,5 +44,18 @@ def add_time(r: Room):
             col3.insert_one({"Time": res})
         else:
             col4.insert_one({"Time": res})
+        return {
+            "result": "exit room {}".format(r.Room),
+            "time use": res
+        }
 
+@app.get("/get/time_used/{room}")
+def show_time_used(room: int):
+    query = {"Room": room}
+    time = col1.find_one(query, {"_id": 0,"Time": 1})
+    res = datetime.now().timestamp() - time["Time"]
+    res = datetime.fromtimestamp(res).strftime("%M:%S")
+    return {
+        "time used": res
+    }
 
