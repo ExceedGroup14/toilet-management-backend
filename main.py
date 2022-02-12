f96e6151f1f29e5665f8a9bf938951ba215c9758
Besto9945 <84808974+Besto9945@users.noreply.github.com>
2022-02-12 17:39:37 +0700

    fix

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from pymongo import MongoClient
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
 import time
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = MongoClient('mongodb://localhost', 27017)
 
